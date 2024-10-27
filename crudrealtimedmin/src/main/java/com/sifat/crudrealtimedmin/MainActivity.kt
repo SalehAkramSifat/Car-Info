79cc5bfd6d7ddaebe7c28699d70458e738dc7c26
Saleh Akram Sifat <salehakramsifat@gmail.com>
2024-10-27 23:36:00 +0600

    Initial Commit

---
+++ crudrealtimedmin/src/main/java/com/sifat/crudrealtimedmin/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.sifat.crudrealtimedmin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sifat.crudrealtimedmin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.upload.setOnClickListener {
+            startActivity(Intent(this, UploadActivity::class.java))
+            finish()
+        }
+
+    }
+}
