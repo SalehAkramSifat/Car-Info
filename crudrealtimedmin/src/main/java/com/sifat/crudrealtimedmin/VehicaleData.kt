3752b8e5273a623d5f5e43e29d203d6ec9a20cb3
Saleh Akram Sifat <salehakramsifat@gmail.com>
2024-10-28 00:05:53 +0600

    Initial Commit

---
+++ crudrealtimedmin/src/main/java/com/sifat/crudrealtimedmin/VehicaleData.kt
@@ -0,0 +1,9 @@
+package com.sifat.crudrealtimedmin
+
+data class VehicaleData(
+    val VehicleOwner: String? = null,
+    val VehicleBrand: String? = null,
+    val VehicleRto: String? = null,
+    val VehicleNumber: String? = null
+
+)
